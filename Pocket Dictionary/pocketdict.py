f0278d42cca83f79cf94137abaf28e809ba3658d
Neha-Deo <72018269+Neha-Deo@users.noreply.github.com>
2021-03-29 17:18:22 +0530

    Create pocketdict.py

---
+++ Pocket Dictionary/pocketdict.py
@@ -0,0 +1,12 @@
+# 1) In Terminal Type : pip install pydictionary
+# 2) Write Code :
+
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+print(dictionary.meaning("indentation"))
+
+# 3) Run Code : python pocketdict.py
+# 4) Output : 
+# {'Noun': ['a concave cut into a surface or edge (as in a coastline', 'the formation of small pits in a surface as a consequence of 
+# corrosion', 'the space left between the margin and the start of an indented line', 'the act of cutting into an edge with toothlike notches or 
+# angular incisions']}
