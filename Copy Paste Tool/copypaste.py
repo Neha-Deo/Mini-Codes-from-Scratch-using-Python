e21bd49accc2f019c4723138c8d7ebff207add1e
Neha-Deo <72018269+Neha-Deo@users.noreply.github.com>
2021-03-29 17:04:18 +0530

    Create copypaste.py

---
+++ Copy Paste Tool/copypaste.py
@@ -0,0 +1,12 @@
+# 1) Install pyperclip library : pip install pyperclip
+# 2) Write Code :
+
+import pyperclip
+pyperclip.copy("Hello Python...")
+pyperclip.paste()
+
+# 3) Run Code : python copypaste.py
+# 4) Press : Ctrl + V
+# 5) Output :
+#   C:\Users\admin\Desktop\Mini-Codes>python copypaste.py
+#   C:\Users\admin\Desktop\Mini-Codes>Hello Python...
