2fc9342bf58377efe920ee85cf5d047eab2c0848
Neha-Deo <72018269+Neha-Deo@users.noreply.github.com>
2021-03-29 17:17:14 +0530

    Create googlesearcher.py

---
+++ Google Search Tool/googlesearcher.py
@@ -0,0 +1,10 @@
+# 1) In terminal Type : pip install google
+# 2) Write Code : 
+
+from googlesearch import search
+
+query="Realme"
+for i in search(query,start=0,pause=2):
+    print(i)
+
+# 3) Run Code : python googlesearcher.py
