2294d0b0ec564f4ee16abe237bbe51ecd572a5c4
Neha-Deo <72018269+Neha-Deo@users.noreply.github.com>
2021-03-29 17:05:39 +0530

    Create stacksearch.py

---
+++ Stack Overflow Search Tool/stacksearch.py
@@ -0,0 +1,4 @@
+# This works like Stack Overflow site used for searching query or errors.
+# 1) In Terminal Type : pip install howdoi
+# 2) In Terminal Run : howdoi your query
+# 3) Example : howdoi how to use stack
